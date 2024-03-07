5a46543258244f02b023cef921f989d008d47822
Ethan Swan <ethanpswan@gmail.com>
2024-03-07 18:03:16 -0500

    Improve prompt friendliness.

--- src/notes_rag/main.py
+++ src/notes_rag/main.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from .prompt import do_query
 from .store import generate_retriever
+from prompt_toolkit import PromptSession
 
-def query():
+
+def prompt():
     if len(sys.argv) < 2:
         print("Missing path argument")
         sys.exit(1)
@@ -13,8 +15,13 @@ def query():
     print("Creating retriever...")
     retriever = generate_retriever(path)
 
+    session = PromptSession()
     while True:
-        query = input("Query: ")
+        try:
+            query = session.prompt("Query: ")
+        except EOFError:
+            print("Exiting...")
+            break
         print("----- Result ----")
         result = do_query(query, retriever, model_type="openai")
         print(result)
--- src/notes_rag/prompt.py
+++ src/notes_rag/prompt.py
@@ -1,4 +1,3 @@
-
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
@@ -15,17 +14,20 @@ Question: {query}
 
 """
 
-def do_query(query: str, retriever: VectorStoreRetriever, model_type: str = 'openai') -> str:
-    if model_type == 'local-small':
+
+def do_query(
+    query: str, retriever: VectorStoreRetriever, model_type: str = "openai"
+) -> str:
+    if model_type == "local-small":
         raise NotImplementedError("Local small model not available")
-    elif model_type == 'local-medium':
+    elif model_type == "local-medium":
         llm = Llama.from_pretrained(
             repo_id="TheBloke/Mistral-3B-v0.1-GGUF",
             filename="mistral-7b-v0.1.Q4_K_M.gguf",
             verbose=False,
         )
         model = LlamaCpp(model_path=llm.model_path, n_ctx=2048)
-    elif model_type == 'openai':
+    elif model_type == "openai":
         model = ChatOpenAI()
     else:
         raise ValueError(f"Unknown model type {model_type}")
